19ae98a490fd7c25cc6434ff64bf8be026fbec2e
Halozhan <juwonhwang0411@gmail.com>
2024-12-09 14:44:00 +0900

    6장 PROJECT 블로그 기획하고 API 만들기

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,45 @@
+package me.shinsunyoung.springbootdeveloper.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.domain.Article;
+import me.shinsunyoung.springbootdeveloper.dto.AddArticleRequest;
+import me.shinsunyoung.springbootdeveloper.dto.UpdateArticleRequest;
+import me.shinsunyoung.springbootdeveloper.repository.BlogRepository;
+
+@RequiredArgsConstructor
+@Service
+public class BlogService {
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toArticle());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(Long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
+    }
+}
