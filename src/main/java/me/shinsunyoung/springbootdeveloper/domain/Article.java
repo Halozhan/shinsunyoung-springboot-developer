903ceeb31f871cdf59cdd2b629289418a44fbd1d
Halozhan <juwonhwang0411@gmail.com>
2024-12-09 15:18:51 +0900

    7장 PROJECT 블로그 화면 구성하기

--- src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
@@ -1,7 +1,14 @@
 package me.shinsunyoung.springbootdeveloper.domain;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +17,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -35,4 +43,12 @@ public class Article {
         this.title = title;
         this.content = content;
     }
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
