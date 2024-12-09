f35af3d4eae172196b7daa33795d567180a26581
Halozhan <juwonhwang0411@gmail.com>
2024-12-09 16:27:25 +0900

    8장 PROJECT 스프링 시큐리티로 로그인/로그아웃, 회원 가입 구현하기

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/service/UserService.java
@@ -0,0 +1,23 @@
+package me.shinsunyoung.springbootdeveloper.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.domain.User;
+import me.shinsunyoung.springbootdeveloper.dto.AddUserRequest;
+import me.shinsunyoung.springbootdeveloper.repository.UserRepository;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
